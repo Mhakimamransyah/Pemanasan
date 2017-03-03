26846a4d53ebd3d0e7f09d64a198ac4e13315814
achmad-dwi <achmaddwisuriyanto@gmail.com>
2017-03-03 22:29:33 +0700

    Update Admin.php
    controller admin sementara

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,8 +2,24 @@
 
   class Admin extends CI_Controller{
   	function index(){
-  		$this->load->view("Admin/Admin_login_view");
+		$this->load->helper('url');
+  		$this->load->view("Admin/index");
   	}
+	
+	function login() {
+		$this->load->helper('url');
+		$this->load->view('Admin/login');
+	}
+	
+	function loginProcess() {
+		$this->load->helper('url');
+		
+		//
+		//
+		//
+		
+		$this->load->view('Admin/index');
+	}
   }
 
 ?>
